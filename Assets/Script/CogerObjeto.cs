2b27a700edf50746cce68b0635b4d61ceaa34273
A01705742 <A01705742@itesm.mx>
2021-06-08 17:23:32 -0500

    prueba-cogerobj

---
+++ Assets/Script/CogerObjeto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogerObjeto : MonoBehaviour
+{
+    public GameObject 
+    
+    void Start()
+    {
+
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
